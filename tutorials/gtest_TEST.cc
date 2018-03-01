64a55dbaab4c7eccf612747e7bc20aae252f3bbf
Jennifer Buehler <jennifer.e.buehler@gmail.com>
2018-03-01 20:07:18 +1100

    Updated gtest tutorial

--- tutorials/gtest_TEST.cc
+++ tutorials/gtest_TEST.cc
@@ -18,6 +18,8 @@
 #include <collision_benchmark/PrimitiveShape.hh>
 #include "test/MultipleWorldsTestFramework.hh"
 
+#include <gazebo/gazebo.hh>
+
 using collision_benchmark::Shape;
 using collision_benchmark::PrimitiveShape;
 
@@ -31,30 +33,58 @@ class MyTest:
 };
 
 //////////////////////////////////////////////////////////////////////////////
-// AABBTestWorldsAgreement with one cylinder primitive and one box primitive
+// Example for physics engine testing.
+// Creates two shapes and adds them to the worlds.
 TEST_F(MyTest, ExampleTest)
 {
-  std::vector<std::string> selectedEngines;
+  // get all supported physics engines
   std::set<std::string> engines =
     collision_benchmark::GetSupportedPhysicsEngines();
-  // run test on all engines
-  selectedEngines.insert(selectedEngines.end(), engines.begin(), engines.end());
-
-  ASSERT_GE(selectedEngines.size(), 2)
+  // make sure there are at least two supported engines
+  ASSERT_GE(engines.size(), 2)
     << "Need at least two physics engines";
 
-  // Model 1
+  // run test on all engines
+  std::vector<std::string> selectedEngines;
+  selectedEngines.insert(selectedEngines.end(), engines.begin(), engines.end());
+
+  // Create shape 1
   std::string modelName1 = "model1";
   Shape::Ptr shape1(PrimitiveShape::CreateBox(2, 2, 2));
-  // Model 2
+  // Create shape 2
   std::string modelName2 = "model2";
   Shape::Ptr shape2(PrimitiveShape::CreateCylinder(1, 3));
 
-  // To run the test in interactive mode,
+  // Running the test in interactive mode will also bring up gzclient
   bool interactive = true;
   InitMultipleEngines(selectedEngines, interactive);
+
+  // Get the world manager
+  ASSERT_NE(GetServer(), nullptr) << "Could not create and start server";
+  GzWorldManager::Ptr worldManager = GetServer()->GetWorldManager();
+  ASSERT_NE(worldManager.get(), nullptr) << "No valid world manager created";
+
+  // load the two shapes into the world
   LoadShape(shape1, modelName1);
   LoadShape(shape2, modelName2);
+
+  // ...
+  // Do your tests here
+  // ...
+
+  // if you run the test in interactive mode, gzclient will not have
+  // been loaded by the time this test finishes, so we will end the
+  // test only once the client is closed.
+  if (interactive)
+  {
+    // GetMultipleWorlds() returns the GazeboMultipleWorlds instance
+    // which runs as the server. This class supports a client in addition
+    // to a server, just like gazebo starts both gzserver and gzclient.
+    while (GetMultipleWorlds()->IsClientRunning())
+    {
+      gazebo::common::Time::MSleep(100);
+    }
+  }
 }
 
 int main(int argc, char**argv)
